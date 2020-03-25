700586694de6ad7d54d9ae5ed7b34bbfa12f741b
FirmanSyah2701 <abdulfirmansyah7@gmail.com>
2020-03-25 21:47:30 +0700

    fix

---
+++ Circuit.java
@@ -0,0 +1,104 @@
+import java.util.ArrayList;
+class Motorcycle {
+    public String name;
+    public int speed;
+    public int fuel;
+    public int maxFuel;
+
+    public Motorcycle(String name, int speed, int fuel, int maxFuel) {
+        this.name    = name;
+        this.speed   = speed;
+        this.fuel    = fuel;
+        this.maxFuel = maxFuel;
+    }
+
+    public void setFuel(int fuel){
+        if(fuel<=0){
+            this.fuel = 0;
+        }else if(fuel>maxFuel){
+            fuel = this.maxFuel;
+        }else{
+            this.fuel = fuel;
+        }
+    }
+
+    @Override
+    public String toString(){
+        return "Name : " + name + "\nspeed : " + speed + "\nfuel : " + fuel + "\nMax Fuel : " + maxFuel;
+    }    
+
+}
+
+public class Circuit {
+    public ArrayList<Motorcycle> motor = new ArrayList<Motorcycle>();
+    public String circuitName;
+    public int circuitLength;
+    public int raceCount;
+
+    public Circuit(String circuitName, int circuitLength,int raceCount) {
+        this.circuitName   = circuitName; 
+        this.circuitLength = circuitLength;
+        this.raceCount     = raceCount; 
+    }
+    
+    public void refuel(int amount){
+        for (Motorcycle mc : motor) {
+            mc.fuel = mc.fuel + amount;
+        }
+    }
+
+    public void addMotorcylce(Motorcycle motorcycle){
+        motor.add(motorcycle);
+    }
+
+    public void doRace(){
+        //process sorting by speed
+        for (int i=0; i < motor.size(); i++) {
+            int dummy = i;      
+            for (int j = i; j < motor.size(); j++) {
+                if (motor.get(dummy).speed <= motor.get(j).speed) {
+                    dummy = j;
+                }
+            }
+            Motorcycle temp = motor.get(dummy);
+            motor.set(dummy, motor.get(i));
+            motor.set(i, temp);
+        }
+        int limitWin = 3;
+        int countMotor = motor.size();
+            for (int k=0; k<motor.size(); k++) {
+                motor.get(k).fuel = motor.get(k).fuel - (circuitLength * raceCount);
+                if(motor.get(k).fuel<0){
+                    motor.remove(motor.get(k));
+                    k--;
+                }else{
+                    System.out.println("<<Race " + (k+1) + " | " + circuitName + ">>");
+                    System.out.println("[ Winner ]");
+                    System.out.println(motor.get(k).toString());
+                }
+            }
+             if(countMotor>=limitWin){
+                 System.out.println("<<Race " + (limitWin) + " | " + circuitName + ">>");
+                 System.out.println("[ Winner ]");
+                 System.out.println("no one complete this race");
+             }
+    }
+
+    public static void main(String[] args) {
+        Motorcycle m1 = new Motorcycle("Andrea Dovizioso", 30, 135, 1000);
+        Motorcycle m2 = new Motorcycle("Marc Marquez", 25, 120, 1000);
+        Circuit c1     = new Circuit("Losail international Circuit", 120, 1);
+        c1.addMotorcylce(m1);
+        c1.addMotorcylce(m2);
+        c1.doRace();
+        System.out.println("");
+        Motorcycle m3 = new Motorcycle("Maverick Vinales", 40, 70, 150);
+        Motorcycle m4 = new Motorcycle("Joan Mir", 25, 45, 225);
+        Motorcycle m5 = new Motorcycle("Johann Zarco", 50, 10, 200);
+        Circuit c2    = new Circuit("TT Circuit Assen", 10, 2);
+        c2.addMotorcylce(m3);
+        c2.addMotorcylce(m4);
+        c2.addMotorcylce(m5);
+        c2.doRace();
+    }
+}
